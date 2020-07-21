3c07d7227773b4872adf5b6d4591d6ed41c25cd3
Hanane_IHOUM <hananihoum2016@gmail.com>
2020-07-21 20:05:49 +0100

    Setup whitenoise for staticfiles delivery

--- api/prod_settings.py
+++ api/prod_settings.py
@@ -11,6 +11,11 @@ TEMPLATE_DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 
 
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 SECRET_KEY = 'n=chr9gt)io#pz!kphevtks5=7lfwenfyf8b^6h*qw1nyg%rpn'
 
 ALLOWED_HOSTS = ['rh-application.herokuapp.com','localhost', '127.0.0.1']
