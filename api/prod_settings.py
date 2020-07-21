64ea7bff1a1018526f74add82af08c19977fa143
Hanane_IHOUM <hananihoum2016@gmail.com>
2020-07-21 19:55:46 +0100

    Configure PostgreSQL

--- api/prod_settings.py
+++ api/prod_settings.py
@@ -1,9 +1,14 @@
+import dj_database_url
+
+
+
 From api.settings import *
 
 DEBUG = False
 
 TEMPLATE_DEBUG = False
 
+DATABASES['default'] = dj_database_url.config()
 
 
 SECRET_KEY = 'n=chr9gt)io#pz!kphevtks5=7lfwenfyf8b^6h*qw1nyg%rpn'
