ff3588b185b30d80cbd5e79a2340e4208b8d3b76
Hanane_IHOUM <hananihoum2016@gmail.com>
2020-07-21 19:46:43 +0100

    Add prod settings

---
+++ api/prod_settings.py
@@ -0,0 +1,11 @@
+From api.settings import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+
+
+SECRET_KEY = 'n=chr9gt)io#pz!kphevtks5=7lfwenfyf8b^6h*qw1nyg%rpn'
+
+ALLOWED_HOSTS = ['rh-application.herokuapp.com','localhost', '127.0.0.1']
